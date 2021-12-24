b89814f8fed048d4c41d69fb5fa26826089f300d
Eugene Scherba <escherba@gmail.com>
2021-12-24 14:21:33 -0800

    allow skipping targets in json2dot

--- json_to_dot.py
+++ json_to_dot.py
@@ -8,12 +8,57 @@ class Id(object):
 
     def __init__(self):
         super().__init__()
-        self._i = 0
+        self._id = 0
 
     @property
-    def i(self):
-        self._i += 1
-        return self._i
+    def id(self):
+        self._id += 1
+        return self._id
+
+
+def print_single_graph(graph, i, skiptargets=None):
+    name_to_node = {}
+
+    def _escape(s):
+        return '"{}"'.format(s.replace('"', r'\"'))
+
+    def _register_node(name, i, fillcolor="none"):
+        if not (name in name_to_node):
+            node = 'n{}'.format(i.id)
+            name_to_node[name] = node
+            print(f'{node}[label={name}, style = "solid,filled", fillcolor="{fillcolor}"]')
+
+    print("subgraph cluster{}{{".format(i.id))
+
+    parents = []
+    for target, deps in graph.items():
+        target_str = _escape(target)
+        parents.append(target_str)
+
+    if skiptargets is None:
+        skiptargets = []
+
+    for target, deps in graph.items():
+        if target not in skiptargets:
+            target_str = _escape(target)
+            _register_node(target_str, i)
+            target_node = name_to_node[target_str]
+        for dep in deps:
+            dep_str = _escape(dep)
+            if dep_str in parents:
+                _register_node(dep_str, i)
+            else:
+                _register_node(dep_str, i, fillcolor="grey")
+            if target not in skiptargets:
+                print('{} -> {}'.format(target_node, name_to_node[dep_str]))
+    print("}")
+
+
+def _parse_args(args):
+    if len(args) != 1:
+        print('# convert a dependency graph stored in JSON format to DOT format')
+        print('{} < deps.json | dot -Tpdf >| workflow.pdf'.format(args[0]))
+        sys.exit(1)
 
 
 def main(args):
@@ -29,53 +74,10 @@ digraph G {
     ]
     """)
     i = Id()
+    skiptargets = [".PHONY", ".SUFFIXES"]
     for graph in json.load(sys.stdin):
-        print_single_graph(graph, i)
+        print_single_graph(graph, i, skiptargets=skiptargets)
     print("}")
-
-
-def print_single_graph(graph, i):
-    name_to_node = {}
-    print("subgraph cluster{}{{".format(i.i))
-    
-    parents = []
-    for target, deps in graph.items():
-        target_str = _escape(target)
-        parents.append(target_str)
-    
-    for target, deps in graph.items():
-        target_str = _escape(target)
-        _register_node(target_str, i, name_to_node)
-        target_node = name_to_node[target_str]
-        for dep_str in (_escape(dep) for dep in deps):
-            if dep_str in parents:
-                _register_node(dep_str, i, name_to_node)
-            else:
-                _register_filled_node(dep_str, i, name_to_node)
-            print('{} -> {}'.format(target_node, name_to_node[dep_str]))
-    print("}")
-
-def _register_node(name, i, name_to_node):
-    if not (name in name_to_node):
-        node = 'n{}'.format(i.i)
-        name_to_node[name] = node
-        print('{}[label={}]'.format(node, name))
-        
-def _register_filled_node(name, i, name_to_node):
-    if not (name in name_to_node):
-        node = 'n{}'.format(i.i)
-        name_to_node[name] = node
-        print('{}[label={}, style = "solid,filled"]'.format(node, name))
-
-def _escape(s):
-    return '"{}"'.format(s.replace('"', r'\"'))
-
-
-def _parse_args(args):
-    if len(args) != 1:
-        print('# convert a dependency graph stored in JSON format to DOT format')
-        print('{} < deps.json | dot -Tpdf >| workflow.pdf'.format(args[0]))
-        sys.exit(1)
 
 
 if __name__ == '__main__':
